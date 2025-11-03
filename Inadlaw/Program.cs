0db430c85cfe3ed098230371d8f0d2e229782e7d
christian <christiannemeno80@gmail.com>
2025-11-03 22:13:27 +0800

    cors config in program cs
    allows any, should be change for deployment ...

--- Inadlaw/Program.cs
+++ Inadlaw/Program.cs
@@ -2,6 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowReactApp",
+            policy => policy.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod());  
+    });
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactApp");
+
 
 app.UseHttpsRedirection();
 
