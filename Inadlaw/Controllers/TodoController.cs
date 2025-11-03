9bdbec22ca9713b0dbf6b7f4a273afd6da58f193
christian <christiannemeno80@gmail.com>
2025-11-03 22:05:22 +0800

    This is what controller looks like , similar to Spring boot

---
+++ Inadlaw/Controllers/TodoController.cs
@@ -0,0 +1,55 @@
+﻿using Inadlaw.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inadlaw.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private static List<TodoItem> todoItems = new List<TodoItem>();
+
+        private static int nextId = 1;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> GetAll()
+        {
+            return Ok(todoItems);
+        }
+
+
+        [HttpPost]
+        public ActionResult<TodoItem> Add(TodoItem todo)
+        {
+            todo.Id = nextId++;
+            todoItems.Add(todo);
+            return CreatedAtAction(nameof(GetAll), new {id = todo.Id}, todo);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, TodoItem updated)
+        {
+            var todo = todoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+
+            todo.Title = updated.Title;
+            todo.IsCompleted = updated.IsCompleted;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var todo = todoItems.FirstOrDefault(t => t.Id == id);
+            if(todo == null) return NotFound();
+            
+            todoItems.Remove(todo);
+
+            return NoContent();
+        }
+
+
+
+    }
+}
