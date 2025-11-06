9d5ae5f241040eb5baed3c75b0ade27022708256
christian <christiannemeno80@gmail.com>
2025-11-06 21:57:35 +0800

    read and added docs for todoitem check it,
    next i should finish up crud

--- Inadlaw/Models/TodoItem.cs
+++ Inadlaw/Models/TodoItem.cs
@@ -2,6 +2,13 @@
 {
     public class TodoItem
     {
+        /**
+         * In spring boot , theres annotation to make it autoincrement
+         * here its automatic , but explicit its ok too
+         * 
+         *      [Key]
+         *      [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+         */
         public int Id { get; set; }
         public string? Title { get; set; }
         public bool IsCompleted { get; set; }
